120d9c96871c8cfe5097e09e6570dcf91c761593
mwhyte <mrmwhyte@gmail.com>
2020-07-17 23:56:36 +0100

    update

--- publish.sh
+++ publish.sh
@@ -7,6 +7,7 @@ rm -rf docs/*
 wget -r -nH -P docs -E -T 2 -np -k http://localhost:2368/
 
 git add docs
-git commit -m 'updating static content: $timestamp'
+git commit -m "updating static content: $timestamp"
 git tag -a $timestamp -m "version $timestamp"
 git push
+git push origin --tags
