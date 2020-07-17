9cbb8ec34a26bdfaf330c1dd81356224ae4e0d7a
mwhyte <mrmwhyte@gmail.com>
2020-07-17 21:23:55 +0100

    update

---
+++ publish.sh
@@ -0,0 +1,12 @@
+
+timestamp=`date "+%Y%m%d-%H%M%S"`
+
+echo "updating static content: $timestamp"
+
+rm -rf docs/*
+wget -r -nH -P docs -E -T 2 -np -k http://localhost:2368/
+
+git add docs
+git commit -m 'updating static content: $timestamp'
+git tag -a $timestamp -m "version $timestamp"
+git push
